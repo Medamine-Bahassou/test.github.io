bfab7b5e385eea406538612d21d83332548a3327
Mohamed Amine BAHASSOU <146652318+Medamine-Bahassou@users.noreply.github.com>
2024-01-25 01:03:02 +0000

    Add files via upload

---
+++ script(modern).js
@@ -0,0 +1,8 @@
+function openForms() {
+    document.getElementById("myform").style.display = "block";
+    document.getElementById("img_option").style.display = "none";
+}
+function closeForm() {
+    document.getElementById("myform").style.display = "none";
+    document.getElementById("img_option").style.display = "block";
+}
